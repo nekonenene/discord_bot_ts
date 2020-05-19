b9593e303cda12f6611fb8c3606980cf5dfa54b0
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2020-05-20 00:26:06 +0900

    ping コマンドの処理

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
-import { Client } from 'discord.js';
+import dayjs from 'dayjs';
+import { Client, Message } from 'discord.js';
 
 const client = new Client();
+const commandPrefix = '/';
+
+const doCommand = (message: Message) => {
+  const args = message.content.slice(commandPrefix.length).split(/ +/);
+  const command = args.shift()!.toLowerCase();
+
+  switch (command) {
+    case 'ping':
+      const currentTime = dayjs();
+      const messageTime = dayjs(message.createdAt);
+      message.channel.send(`ポンです！ こっちに来るまで${currentTime.diff(messageTime) / 1000}秒かかってました！`);
+      break;
+  }
+}
 
 client.on('ready', () => {
   console.log(`${client.user?.username} の準備ができました！`);
 });
 
-client.on('message', (msg) => {
-  if (msg.content === 'ping') {
-    msg.reply('pong').catch(err => {
-      console.error(err);
-    });
+client.on('message', message => {
+  if (message.author.bot) { return; } // bot自身の投稿に反応しないように
+
+  // コマンドの処理
+  if (message.content.startsWith(commandPrefix)) {
+    doCommand(message);
   }
 });
 
